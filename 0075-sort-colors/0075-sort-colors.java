45660942a6c4bcaf84e43222a114d7a16a8f506a
Ashish Verma <153390071+ashishverma153@users.noreply.github.com>
2026-07-09 14:42:05 +0530

    Time: 0 ms (100%), Space: 42.1 MB (99.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,20 @@
-import java.util.*;
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int max =2;
+        int[] maxsize = new int[max+1];
+
+        for(int i=0;i<nums.length;i++){
+            int value = nums[i];
+            maxsize[value]++;
+        }
+
+        int index=0;
+        for(int i=0;i<=max;i++){
+            while(maxsize[i]>0){
+                nums[index]=i;
+                index++;
+                maxsize[i]--;
+            }
+        }
     }
 }
